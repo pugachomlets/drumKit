098fad6fbe589af16465fe2a2bf99a9e1ffc0f25
Daniel Iha <104171368+pugachomlets@users.noreply.github.com>
2023-09-27 15:29:43 -0500

    Add files via upload

---
+++ drumKit/index.js
@@ -0,0 +1,108 @@
+//To handle on click button simply just using a switch case
+
+function handleClick(){
+    var buttonInnerHTML = this.innerHTML;
+    handleKey(buttonInnerHTML) //to help with animation connection
+    buttonAnimation(buttonInnerHTML);
+    switch (buttonInnerHTML){
+        case "w": 
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+        case "a":
+        var kickbass = new Audio("sounds/kick-bass.mp3");
+        kickbass.play();
+        break;
+        case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+        case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+        case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+        case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+        case "l":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        default:
+    }
+
+}
+
+//Get key input
+document.addEventListener("keypress", function(event){
+    handleKey(event.key);
+    buttonAnimation(event.key);
+});
+
+//Handle keyboard press
+function handleKey(key){
+    switch (key){
+        case "w": 
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+        case "a":
+        var kickbass = new Audio("sounds/kick-bass.mp3");
+        kickbass.play();
+        break;
+        case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+        case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+        case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+        case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+        case "l":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        default:
+    }
+
+}
+
+
+
+//Add the event listener to all drums
+for (var i=0; i<document.querySelectorAll(".drum").length; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    //timeout refer to https://www.w3schools.com/jsref/met_win_settimeout.asp
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+}
+
+
+
+
+
